13700f06251333720911096f31ab9e1a8da8a37a
Sanskar Bosmia <sanskarbosmia@gmail.com>
2025-06-07 16:50:46 +0530

    Refactored code for formatting

--- PennyPilot.Frontend/src/app/core/interceptors/auth.interceptor.ts
+++ PennyPilot.Frontend/src/app/core/interceptors/auth.interceptor.ts
@@ -1,22 +1,30 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
-import { inject, Injectable } from "@angular/core";
-import { TokenService } from "../services/token.service";
-import { Observable } from "rxjs";
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { TokenService } from '../services/token.service';
+import { Observable } from 'rxjs';
 
 @Injectable()
-export class AuthInterceptor implements HttpInterceptor{
-    private tokenService = inject(TokenService);
+export class AuthInterceptor implements HttpInterceptor {
+  private tokenService = inject(TokenService);
 
-    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        const token = this.tokenService.getToken();
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    const token = this.tokenService.getToken();
 
-        if(token){
-            const cloned = req.clone({
-                headers: req.headers.set('Authorization', `Bearer ${token}`)
-            });
-            return next.handle(cloned);
-        }
-
-        return next.handle(req);
+    if (token) {
+      const cloned = req.clone({
+        headers: req.headers.set('Authorization', `Bearer ${token}`),
+      });
+      return next.handle(cloned);
     }
+
+    return next.handle(req);
+  }
 }
--- PennyPilot.Frontend/src/app/core/services/token.service.ts
+++ PennyPilot.Frontend/src/app/core/services/token.service.ts
@@ -1,23 +1,22 @@
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
 
 const TOKEN_Key = 'auth_token';
 
-@Injectable({providedIn:'root'})
-export class TokenService{
-    saveToken(token:string): void{
-        localStorage.setItem(TOKEN_Key, token);
-    }
+@Injectable({ providedIn: 'root' })
+export class TokenService {
+  saveToken(token: string): void {
+    localStorage.setItem(TOKEN_Key, token);
+  }
 
-    getToken(): string | null{
-        return localStorage.getItem(TOKEN_Key);
-    }
+  getToken(): string | null {
+    return localStorage.getItem(TOKEN_Key);
+  }
 
-    removeToken() : void{
-        localStorage.removeItem(TOKEN_Key);
-    }
+  removeToken(): void {
+    localStorage.removeItem(TOKEN_Key);
+  }
 
-    isLoggedIn(): boolean{
-        return !!this.getToken();
-    }
-
+  isLoggedIn(): boolean {
+    return !!this.getToken();
+  }
 }
