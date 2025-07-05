e0d0eb17f42f49aa6e942063a39217dd858ccd33
Sanskar Bosmia <sanskarbosmia@gmail.com>
2025-07-05 16:42:11 +0530

    Added styling of net savings card

--- PennyPilot.Frontend/src/app/dashboard/dashboard.component.ts
+++ PennyPilot.Frontend/src/app/dashboard/dashboard.component.ts
@@ -52,17 +52,22 @@ export class DashboardComponent {
     { title: 'Net Savings', amount: 0, icon: 'savings', color: '#6366F1' },
   ];
 
+  getNetSavingsColor(amount: number): string {
+    if (amount === 0) return '#111827'; // neutral color for zero savings
+    return amount >= 0 ? 'rgb(25 153 30)' : 'rgb(221 27 27)'; // green or red
+  }
+
   constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
-   this.reloadSummaryCards();
+    this.reloadSummaryCards();
   }
 
   logout() {
     this.authService.logout();
   }
 
-  reloadSummaryCards(){
+  reloadSummaryCards() {
     this.summaryCardsLoading = true;
     this.cardsService.getSummaryCardsData(this.dashboardFilter).subscribe({
       next: (res: ApiResponse<SummaryCardsResponse>) => {
@@ -70,27 +75,51 @@ export class DashboardComponent {
         const totalExpenses = res.data?.totalExpenses?.value;
         const netSavings = res.data?.netSavings?.value;
 
-        if(res.success){
+        if (res.success) {
           this.summaryCards = [
-            { title: 'Total Income', amount: totalIncome || 0, icon: 'trending_up', color: '#10B981' },
-            { title: 'Total Expenses', amount: totalExpenses || 0, icon: 'trending_down', color: '#EF4444' },
-            { title: 'Net Savings', amount: netSavings || 0, icon: 'savings', color: '#6366F1' },
+            {
+              title: 'Total Income',
+              amount: totalIncome || 0,
+              icon: 'trending_up',
+              color: '#10B981',
+            },
+            {
+              title: 'Total Expenses',
+              amount: totalExpenses || 0,
+              icon: 'trending_down',
+              color: '#EF4444',
+            },
+            {
+              title: 'Net Savings',
+              amount: netSavings || 0,
+              icon: 'savings',
+              color: '#6366F1',
+            },
           ];
         }
       },
       error: () => {
         this.summaryCardsError = 'Failed to load summary cards data';
         this.summaryCardsLoading = false;
-      }
+      },
     });
   }
 
-  onDateRangeChange(range: { start: Date | null; end: Date | null; granularity: string }) {
-    this.dashboardFilter = { ...this.dashboardFilter, startDate: range.start, endDate: range.end,granularity: range.granularity };
+  onDateRangeChange(range: {
+    start: Date | null;
+    end: Date | null;
+    granularity: string;
+  }) {
+    this.dashboardFilter = {
+      ...this.dashboardFilter,
+      startDate: range.start,
+      endDate: range.end,
+      granularity: range.granularity,
+    };
     this.reloadAllWidgets();
   }
 
-   reloadAllWidgets() {
+  reloadAllWidgets() {
     if (this.chartsComp) {
       this.chartsComp.reloadCharts(this.dashboardFilter);
     }
